8d141f1dfcd6cf6d56c5df67c73d24fa4e6beed1
NXTORBIS TECHNOLOGIES <nxtorbis@gmail.com>
2025-03-02 03:15:52 +0530

    base version for the landing page of nxtorbis website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, RouterModule, CarouselComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { AboutComponent } from './components/about/about.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 
 export const routes: Routes = [ { path: '', component: HomeComponent, pathMatch: 'full' },
 { path: 'about', component: AboutComponent },
 { path: 'products', component: ProductsComponent },
 { path: 'contact', component: ContactComponent },
-{ path: 'privacy-policy', component: PrivacyPolicyComponent }
+{ path: 'pricing', component: PricingComponent }
 ];
