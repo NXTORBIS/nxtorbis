920dcb5f92796cbc04730c5cbc1a31dbee18b30a
NXTORBIS TECHNOLOGIES <nxtorbis@gmail.com>
2025-03-03 01:51:53 +0530

    brought NxtOrbis website to some shape

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
+// import { AboutComponent } from './components/about/about.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ServicesComponent } from './components/services/services.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 
 
-export const routes: Routes = [ { path: '', component: HomeComponent, pathMatch: 'full' },
-{ path: 'about', component: AboutComponent },
+export const routes: Routes = [ { path: '', pathMatch: 'full', redirectTo: 'home' },
+{path: 'home', component: AboutusComponent},
+{ path: 'about', component: HomeComponent },
 { path: 'products', component: ProductsComponent },
+{ path: 'services', component: ServicesComponent },
 { path: 'contact', component: ContactComponent },
 { path: 'pricing', component: PricingComponent }
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
